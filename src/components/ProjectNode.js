e22204fc050016ae6f916030912bd3098297265b
Siu <210014@Ansi8039-MKT.nyhq.nytint.com>
2019-01-23 12:13:16 -0500

    simple drag drop events

--- src/components/ProjectNode.js
+++ src/components/ProjectNode.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 
 class ProjectsNode extends Component {
   render() {
+    const {name, dragStartHandler} = this.props;
+
     return (
-      <div className="project-node" draggable="true">
-        <span>{this.props.name}</span>
+      <div className="project-node" 
+        draggable="true"
+        name={name}
+        onDragStart={dragStartHandler}>
+        <span>{name}</span>
       </div>
     );
   }
--- src/components/ProjectsContainer.js
+++ src/components/ProjectsContainer.js
@@ -5,13 +5,36 @@ import ProjectNode from './ProjectNode';
 import projectsData from '../data/projects.json';
 
 class ProjectsContainer extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			projects: projectsData
+		};
+	}
+
+	handleDragStart = e => {
+		console.log('dragging!', e.target.getAttribute('name'));
+		e.dataTransfer.setData('projectName', e.target.getAttribute('name'));
+	}
+
+	handleDrop = e => {
+		e.preventDefault();
+		const projectToDrop = e.dataTransfer.getData('projectName');
+		console.log('dropping!', projectToDrop);
+		// e.target.appendChild(document.querySelector(`[name="${projectToDrop}"]`));
+	}
+
   render() {
     return (
-      <section id="projects-container">
+      <section id="projects-container"
+      	onDrop={e => this.handleDrop(e)}
+      	onDragOver={e => e.preventDefault()}>
       	{
       		projectsData.map((project,i) => (
       			<ProjectNode key={i} 
-      				name={project.name}/>
+      				name={project.name}
+      				dragStartHandler={e => this.handleDragStart(e)}/>
 	      	))	
       	}
       </section>
@@ -20,3 +43,4 @@ class ProjectsContainer extends Component {
 }
 
 export default ProjectsContainer;
+
