83a32c05eda51c6b1d2dc7060b4f954173575491
Siu <210014@Ansi8039-MKT.nyhq.nytint.com>
2019-01-25 15:12:45 -0500

    create input element to add task

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 
 import ProjectsContainer from './components/ProjectsContainer';
+import ProjectInput from './components/ProjectInput';
+
+import projectsData from './data/projects.json';
 
 class App extends Component {
+  /*constructor() {
+    super;
+
+    this.state = {
+      projects: projectsData.map(project => project.name),
+      ignoringDragOver: false
+    };
+  }*/
+
   render() {
     return (
       <div className="App">
         <header>
           <h1>To-Do Drag-Drop</h1>
         </header>
-        <ProjectsContainer></ProjectsContainer>
+        <ProjectInput />
+        <ProjectsContainer />
       </div>
     );
   }
--- src/components/ProjectInput.js
+++ src/components/ProjectInput.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 
 class ProjectInput extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: ""
+    };
+  }
+
   render() {
     return (
       <section id="project-input">
-        <p>hello</p> 
+        <label>Task Name:</label>
+        <input type="text" placeholder="XYZ Task"
+          onKeyUp={e => {
+            this.setState({value: e.target.value})
+          }}/>
+        <button 
+          onClick={() => {
+            console.log('Added task:', this.state.value);
+        }}>Add Task</button> 
       </section>
     );
   }
