908a5d3cb850231509ed65a2d39b664a89f665cb
Siu <210014@Ansi8039-MKT.nyhq.nytint.com>
2019-01-25 12:47:48 -0500

    refactor ProjectNode to function component; create dummy ProjectInput component

---
+++ src/components/ProjectInput.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProjectInput extends Component {
+  render() {
+    return (
+      <section id="project-input">
+        <p>hello</p> 
+      </section>
+    );
+  }
+}
+
+export default ProjectInput;
--- src/components/ProjectNode.js
+++ src/components/ProjectNode.js
@@ -1,22 +1,20 @@
 import React, { Component } from 'react';
 
-class ProjectsNode extends Component {
-  render() {
-    const {name, dragStartHandler, dragOverHandler, dragEndHandler, nodeStyles} = this.props;
+const ProjectsNode = props => {
+  const {name, dragStartHandler, dragOverHandler, dragEndHandler, nodeStyles} = props; 
 
-    return (
-      <div className="project-node" 
-        draggable="true"
-        name={name}
-        onDragStart={dragStartHandler}
-        onDragOver={dragOverHandler}
-        onDragEnd={dragEndHandler}
-        style={nodeStyles}>
-        <span>{name}</span>
-        <i className="fas fa-arrows-alt" aria-hidden="true"></i>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="project-node" 
+      draggable="true"
+      name={name}
+      onDragStart={dragStartHandler}
+      onDragOver={dragOverHandler}
+      onDragEnd={dragEndHandler}
+      style={nodeStyles}>
+      <span>{name}</span>
+      <i className="fas fa-arrows-alt" aria-hidden="true"></i>
+    </div>
+  );
+};
 
 export default ProjectsNode;
