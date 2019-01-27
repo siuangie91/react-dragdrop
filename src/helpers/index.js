261f7e2f59d26cf93be2d6836f9f8c8ab6acc1c4
siuangie91 <siuangie91@gmail.com>
2019-01-27 16:10:12 -0500

    put all console logs behind dev_console cookie

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,25 @@
-export function logMsg(...msgs) {
-	console.log('[DRAG-DROP MSG]:', ...msgs);		
+let dev_console = getCookie("dev_console");
+if(dev_console) { // overwrite cookie and set to expire in 24 hrs
+  document.cookie = "dev_console=true; max-age=60*60*24;"
+}
+export function logMsg(...msgs) { // hide all console msgs behind a cookie
+  if(dev_console) {
+    console.log("[DEV CONSOLE MSG]: ", ...msgs);
+  }
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) === ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) === 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
 }
