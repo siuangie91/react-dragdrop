9fd44dacd32a8d4eda4edd23b896ae0ab5cb21ab
Siu <210014@Ansi8039-MKT.nyhq.nytint.com>
2019-02-19 11:27:44 -0500

    format

--- src/components/ProjectsContainer.js
+++ src/components/ProjectsContainer.js
@@ -4,57 +4,57 @@ import ProjectContext from '../context/ProjectContext';
 import ProjectNode from './ProjectNode';
 import MaxLengthModal from './MaxLengthModal';
 
-import {logMsg, placeCaretAtEnd, projectNameMaxLength} from '../helpers';
+import { logMsg, placeCaretAtEnd, projectNameMaxLength } from '../helpers';
 
 class ProjectsContainer extends Component {
-	static contextType = ProjectContext;
+  static contextType = ProjectContext;
 
-	constructor() {
-		super();
+  constructor() {
+    super();
 
-		this.state = {
-			ignoringDragOver: false,
-			isDragging: false,
-			reachedCharLimit: false
-		};
+    this.state = {
+      ignoringDragOver: false,
+      isDragging: false,
+      reachedCharLimit: false
+    };
 
-		this.projectNodeRefs = [];
-		this.setProjectNodeRef = (elem, i) => {
-			this.projectNodeRefs[i] = elem;
-		};
+    this.projectNodeRefs = [];
+    this.setProjectNodeRef = (elem, i) => {
+      this.projectNodeRefs[i] = elem;
+    };
 
-		this.nodeCopyContainer = {};
-		this.newName = "";
-	}
+    this.nodeCopyContainer = {};
+    this.newName = "";
+  }
 
   handleDragStart = (e, idx) => {
-		this.projectToMove = this.context.projectsData[idx];
-		logMsg('dragging!', this.projectToMove);
+    this.projectToMove = this.context.projectsData[idx];
+    logMsg('dragging!', this.projectToMove);
 
-		e.dataTransfer.effectAllowed = "move";
-		logMsg('handleDragStart dropEffect', e.dataTransfer.dropEffect);
-		e.dataTransfer.setData('text/html', e.target);
-	}
+    e.dataTransfer.effectAllowed = "move";
+    logMsg('handleDragStart dropEffect', e.dataTransfer.dropEffect);
+    e.dataTransfer.setData('text/html', e.target);
+  }
 
-	handleDragOver = (e, idx) => {
-		// prevent default so that drop is allowed (https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API/Drag_operations#droptargets)
-		e.preventDefault();
-		e.dataTransfer.dropEffect = "move";
+  handleDragOver = (e, idx) => {
+    // prevent default so that drop is allowed (https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API/Drag_operations#droptargets)
+    e.preventDefault();
+    e.dataTransfer.dropEffect = "move";
 
-		const draggedOverItem = this.context.projectsData[idx];
-		// logMsg('draggedOverItem', draggedOverItem);
+    const draggedOverItem = this.context.projectsData[idx];
+    // logMsg('draggedOverItem', draggedOverItem);
 
     // if the item is dragged over itself, ignore
     if (this.projectToMove === draggedOverItem) {
-    	if(!this.state.ignoringDragOver) { // only if !ignoringDragOver to prevent firing too many events
-    		logMsg('Ignoring! Dragging over the same item!');
-    		
-    		this.setState({ignoringDragOver: true});
-      	return;	
-    	}   	
-		}
-		
-		const {projectsData, updateProjects} = this.context;
+      if (!this.state.ignoringDragOver) { // only if !ignoringDragOver to prevent firing too many events
+        logMsg('Ignoring! Dragging over the same item!');
+
+        this.setState({ ignoringDragOver: true });
+        return;
+      }
+    }
+
+    const { projectsData, updateProjects } = this.context;
 
     // filter out the currently dragged item
     let projects = projectsData.filter(project => project !== this.projectToMove);
@@ -63,140 +63,140 @@ class ProjectsContainer extends Component {
 
     // set the projectsData in the contextValue to the new list order
     updateProjects(projects);
-	}
+  }
 
-	handleDragEnd = e => {
-		e.preventDefault();
-		e.dataTransfer.effectAllowed = "move";
-		e.dataTransfer.dropEffect = "move";
+  handleDragEnd = e => {
+    e.preventDefault();
+    e.dataTransfer.effectAllowed = "move";
+    e.dataTransfer.dropEffect = "move";
 
     this.projectToMove = null;
-    this.setState({ignoringDragOver: false}); // reset
-	}
+    this.setState({ ignoringDragOver: false }); // reset
+  }
 
   deleteHandler = id => {
-  	this.context.deleteProject(id);
-	}
-	
-	editHandler = idx => {
-		this.nodeCopyContainer = this.projectNodeRefs[idx];
-		this.nodeCopyContainer.setAttribute('contenteditable', true);
+    this.context.deleteProject(id);
+  }
 
-		placeCaretAtEnd(this.nodeCopyContainer);
-	}
+  editHandler = idx => {
+    this.nodeCopyContainer = this.projectNodeRefs[idx];
+    this.nodeCopyContainer.setAttribute('contenteditable', true);
 
-	handleKeyup = e => {
-		// logMsg('handleKeyup key', e.key);
-		const {nodeCopyContainer, setNewNameAsInnertext} = this;
-		
-		setNewNameAsInnertext();
+    placeCaretAtEnd(this.nodeCopyContainer);
+  }
 
-		if(e.key === "Escape") {
-			nodeCopyContainer.blur();
-		}
-		else if(nodeCopyContainer.innerText.length >= projectNameMaxLength) {
-			this.setState({reachedCharLimit: true});
+  handleKeyup = e => {
+    // logMsg('handleKeyup key', e.key);
+    const { nodeCopyContainer, setNewNameAsInnertext } = this;
 
-			nodeCopyContainer.blur();
-		}
-		else {
-			setNewNameAsInnertext();
-			// logMsg('newName', newName);
-		}
-	}
+    setNewNameAsInnertext();
 
-	handleEnterKey = e => {
-		if(e.key === "Enter") {
-			e.preventDefault();
-			this.setNewNameAsInnertext();
-			this.nodeCopyContainer.blur();
-		}
-	}
+    if (e.key === "Escape") {
+      nodeCopyContainer.blur();
+    }
+    else if (nodeCopyContainer.innerText.length >= projectNameMaxLength) {
+      this.setState({ reachedCharLimit: true });
 
-	handleBlur = (idx, id) => {
-		this.nodeCopyContainer.setAttribute('contenteditable', false);
+      nodeCopyContainer.blur();
+    }
+    else {
+      setNewNameAsInnertext();
+      // logMsg('newName', newName);
+    }
+  }
 
-		const {projectsData, updateProjects} = this.context;
+  handleEnterKey = e => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      this.setNewNameAsInnertext();
+      this.nodeCopyContainer.blur();
+    }
+  }
 
-		let projToEdit = projectsData.find(item => item.id === id);
-		projToEdit.name = this.newName; // update the name
+  handleBlur = (idx, id) => {
+    this.nodeCopyContainer.setAttribute('contenteditable', false);
 
-		const projectList = projectsData;
-		projectList.splice(idx, 1, projToEdit); // replace it in the list
+    const { projectsData, updateProjects } = this.context;
 
-		updateProjects(projectList);	
-	}
+    let projToEdit = projectsData.find(item => item.id === id);
+    projToEdit.name = this.newName; // update the name
 
-	setNewNameAsInnertext = () => {
-		this.newName = this.nodeCopyContainer.innerText;
-	}
+    const projectList = projectsData;
+    projectList.splice(idx, 1, projToEdit); // replace it in the list
+
+    updateProjects(projectList);
+  }
+
+  setNewNameAsInnertext = () => {
+    this.newName = this.nodeCopyContainer.innerText;
+  }
 
   setNodeColor = idx => {
-  	const numNodes = this.context.projectsData.length;
+    const numNodes = this.context.projectsData.length;
 
-  	const percent = 1 / numNodes;
-  	const degreeTransparency = percent * idx;
-  	// set max transparency to 10%
-  	const maxTransparency = 0.1;
-  	const alpha = Math.max(maxTransparency, 1 - degreeTransparency);
+    const percent = 1 / numNodes;
+    const degreeTransparency = percent * idx;
+    // set max transparency to 10%
+    const maxTransparency = 0.1;
+    const alpha = Math.max(maxTransparency, 1 - degreeTransparency);
 
-  	return {
-  		backgroundColor: `rgba(58,96,158,${alpha})`,
-  		color: `#${(alpha <= 0.4) ? '000' : 'fff'}`
-  	};
-	}
-	
-	dismissModal = () => {
-		this.setState({reachedCharLimit: false});
-	}
+    return {
+      backgroundColor: `rgba(58,96,158,${alpha})`,
+      color: `#${(alpha <= 0.4) ? '000' : 'fff'}`
+    };
+  }
+
+  dismissModal = () => {
+    this.setState({ reachedCharLimit: false });
+  }
 
   render() {
-		// logMsg('ProjectsContainer this.context', this.context);
-  	return (
-  		<ProjectContext.Consumer name="ProjectContextConsumer.ProjectsContainer">
-  			{
-  				value => {
-  					// logMsg('ProjectsContainer values', value);
+    // logMsg('ProjectsContainer this.context', this.context);
+    return (
+      <ProjectContext.Consumer name="ProjectContextConsumer.ProjectsContainer">
+        {
+          value => {
+            // logMsg('ProjectsContainer values', value);
 
             return (
-            	<section id="projects-container">
-            		{
-            			(value.projectsData.length === 0) ? 
-            				<p className="error">You have no tasks!</p>
-            				: ""
-								}
-								{
-									this.state.reachedCharLimit ? 
-										<MaxLengthModal modalClickHandler={this.dismissModal} />
-										: ""
-								}
-				      	<ol>
-				      		{
-					      		value.projectsData.map((project,i) => (
-					      			<li key={i}>
-					      				<ProjectNode
-													id={project.id}
-													name={project.name}
-													projectRef={elem => this.setProjectNodeRef(elem, i)}
-						      				dragStartHandler={e => this.handleDragStart(e, i)}
-						      				dragOverHandler={e => this.handleDragOver(e, i)}
-													dragEndHandler={e => this.handleDragEnd(e)}
-													editHandler={() => this.editHandler(i)}
-													deleteHandler={() => this.deleteHandler(project.id)}
-													keyupHandler={e => this.handleKeyup(e)}
-													enterKeyHandler={e => this.handleEnterKey(e)}
-													blurHandler={() => this.handleBlur(i, project.id)}
-						      				nodeStyles={this.setNodeColor(i)} />
-					      			</li>
-						      	))	
-					      	}
-				      	</ol>
-				      </section>
+              <section id="projects-container">
+                {
+                  (value.projectsData.length === 0) ?
+                    <p className="error">You have no tasks!</p>
+                    : ""
+                }
+                {
+                  this.state.reachedCharLimit ?
+                    <MaxLengthModal modalClickHandler={this.dismissModal} />
+                    : ""
+                }
+                <ol>
+                  {
+                    value.projectsData.map((project, i) => (
+                      <li key={i}>
+                        <ProjectNode
+                          id={project.id}
+                          name={project.name}
+                          projectRef={elem => this.setProjectNodeRef(elem, i)}
+                          dragStartHandler={e => this.handleDragStart(e, i)}
+                          dragOverHandler={e => this.handleDragOver(e, i)}
+                          dragEndHandler={e => this.handleDragEnd(e)}
+                          editHandler={() => this.editHandler(i)}
+                          deleteHandler={() => this.deleteHandler(project.id)}
+                          keyupHandler={e => this.handleKeyup(e)}
+                          enterKeyHandler={e => this.handleEnterKey(e)}
+                          blurHandler={() => this.handleBlur(i, project.id)}
+                          nodeStyles={this.setNodeColor(i)} />
+                      </li>
+                    ))
+                  }
+                </ol>
+              </section>
             );
-  				}
-  			}
-  		</ProjectContext.Consumer>
-  	);
+          }
+        }
+      </ProjectContext.Consumer>
+    );
   }
 }
 
